c1220d4b594f0274ba9edd56e106993f6b088bbc
章芝源 <610925874@qq.com>
2016-01-01 17:13:08 +0800

    设置圆和矩形

--- 绘制图形/绘制图形/shape.m
+++ 绘制图形/绘制图形/shape.m
@@ -12,8 +12,38 @@
 
 - (void)drawRect:(CGRect)rect
 {
-    [self triangle];
+//    [self triangle];
+//    [self setupImage];
+    [self setupRect];
+    
 }
+
+//画矩形&圆
+- (void)setupRect
+{
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+
+//    UIBezierPath *bzrPath = [UIBezierPath bezierPathWithRect:CGRectMake(0, 0, 100, 200)];
+    UIBezierPath *bzrPath = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, 100, 100)];
+    CGContextAddPath(ctx, bzrPath.CGPath);
+    [[UIColor redColor]setFill];
+    [[UIColor blueColor]setStroke];
+//    CGContextStrokePath(ctx);
+    CGContextDrawPath(ctx, kCGPathFillStroke);
+}
+
+
+//画图片失败
+- (void)setupImage
+{
+    CGContextRef cxt = UIGraphicsGetCurrentContext();
+//    CGMutablePathRef path = CGPathCreateMutable();
+    UIImage *image = [UIImage imageNamed:@"普京"];
+    CGImageRef imageRef = (__bridge CGImageRef)([UIImage imageWithCGImage:(__bridge CGImageRef _Nonnull)(image)]);
+    CGContextDrawImage(cxt, CGRectMake(0, 0, 100, 100), imageRef);
+}
+
+
 
 //三角形
 - (void)triangle
@@ -32,11 +62,12 @@
 
     CGContextAddPath(cxt, path);
     //设置颜色
-//        [[UIColor redColor]setStroke];
+        [[UIColor redColor]setStroke];
     [[UIColor yellowColor]setFill];
     //    [[UIColor blueColor]set];
     
 //    CGContextStrokePath(cxt);//stroke是描边
-    CGContextFillPath(cxt);
+//    CGContextFillPath(cxt);
+    CGContextDrawPath(cxt, kCGPathFillStroke);
 }
 @end
